541f3d34b54c0d11958ae689273cbdf7c75d53e5
Edward Essing <edwardessing@gmail.com>
2018-01-25 12:59:01 +1100

    Changes
    Changes

---
+++ _plugins/cats.rb
@@ -0,0 +1,20 @@
+# module Jekyll
+#   class CatsGenerator < Generator
+#     def generate(site)
+#       cats = []
+#       for ent in Dir.entries("assets/selected")
+#         name = File.basename(ent, File.extname(ent))
+#         if name[0] == "."
+#           next
+#         end
+#         cat = {
+#           "name" => name,
+#           "path" => "selected/" + ent}
+#         cats << cat
+#       end
+#       cats.sort! { | a, b | a["name"] <=> b["name"] }
+
+#       site.data['selected'] = cats
+#     end
+#   end
+# end
